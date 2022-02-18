27e4e011b44059fdbd56b91d61ba619c697ed73b
tianqinghai <qhtian@qysoft.cn>
2022-02-18 09:31:00 +0800

    update

--- sw.js
+++ sw.js
@@ -2,36 +2,36 @@ importScripts('/i-home/_nuxt/workbox.6041bd81.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/i-home/_nuxt/7d363dd.js",
-    "revision": "8f3d3998dd4428d01752ab584c8ed5fc"
+    "url": "/i-home/_nuxt/0347ad8.js",
+    "revision": "76b4301c2b6391f33b6f77a6ee2de20c"
   },
   {
-    "url": "/i-home/_nuxt/96c587c.js",
-    "revision": "0d090a96613f88ce30bd69cfcdff5774"
+    "url": "/i-home/_nuxt/2952490.js",
+    "revision": "3b3f9c322eeac132a27b1eedebc8ba52"
   },
   {
-    "url": "/i-home/_nuxt/a1120fe.js",
-    "revision": "2ceab28de75bedeeaf0eebeb4695b248"
+    "url": "/i-home/_nuxt/5a4fbb4.js",
+    "revision": "4fb04fb98f9c0e60fb0ce1de78cf1934"
   },
   {
-    "url": "/i-home/_nuxt/aac84dc.js",
-    "revision": "64b50fcbabd30880325f5fe377137b8d"
+    "url": "/i-home/_nuxt/9551221.js",
+    "revision": "551c71ab0885f45c80bb627b365f3684"
   },
   {
-    "url": "/i-home/_nuxt/d942e39.js",
-    "revision": "6df2d8d0cb0ba02b1e34aa93e130cee2"
+    "url": "/i-home/_nuxt/a56fac2.js",
+    "revision": "749d6869e2b2b2044d9a45e1d69bfd44"
   },
   {
-    "url": "/i-home/_nuxt/eb175bd.js",
-    "revision": "6d1115d2c9492527eecd8af1940a4369"
+    "url": "/i-home/_nuxt/b4c9892.js",
+    "revision": "54ed253953995aab0a7f03c2e4c275eb"
   },
   {
-    "url": "/i-home/_nuxt/faf6275.js",
-    "revision": "d444e888f03d58e5a3a4bb1017be8525"
+    "url": "/i-home/_nuxt/e8dc8f6.js",
+    "revision": "d38a32be31c27d911ae386c2af98472a"
   },
   {
-    "url": "/i-home/_nuxt/ff182b9.js",
-    "revision": "67c3546e74b176bd715f0503f1f8217e"
+    "url": "/i-home/_nuxt/feb5d4b.js",
+    "revision": "a6ee86ec0175501737fdb5a4410d504a"
   }
 ], {
   "cacheId": "i-app",
